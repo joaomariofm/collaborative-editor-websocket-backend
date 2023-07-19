e8c337f2f25bb38ec3c25b1556a2999a82c9cb6d
Amadeu Lee <amadeu.lee@townsq.com.br>
2023-07-18 23:42:42 -0300

    change to gradle project

---
+++ src/main/java/com/example/collaborativeeditorwebsocketbackend/service/DoOperationService.java
@@ -0,0 +1,11 @@
+package com.example.collaborativeeditorwebsocketbackend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DoOperationService {
+
+    public void handleMultipleOperation() {
+
+    }
+}
