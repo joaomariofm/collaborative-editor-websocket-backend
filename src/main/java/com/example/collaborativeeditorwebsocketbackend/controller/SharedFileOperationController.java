7f512a9e1cbd61f4873f8bedb4ecc1efa875e9df
João Mário <joaomf17p2@gmail.com>
2023-07-19 21:46:46 -0300

    returning altered version to all clients

--- src/main/java/com/example/collaborativeeditorwebsocketbackend/controller/SharedFileOperationController.java
+++ src/main/java/com/example/collaborativeeditorwebsocketbackend/controller/SharedFileOperationController.java
@@ -4,7 +4,10 @@ import com.example.collaborativeeditorwebsocketbackend.entity.Operation;
 import com.example.collaborativeeditorwebsocketbackend.entity.SharedFile;
 import com.example.collaborativeeditorwebsocketbackend.service.DoOperationService;
 import java.net.InetAddress;
+import java.util.Vector;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -16,9 +19,11 @@ public class SharedFileOperationController {
     }
 
     @MessageMapping("/doOperation")
-    public void doOperation(Operation operation) throws Exception {
+    @SendTo("/topic/versionUpdate")
+    public Vector<Character> doOperation(Operation operation) throws Exception {
         operation.setiOperation(Operation.getOperationInterface(operation.getType()));
         operationService.handleMultipleOperation(operation);
-        System.out.println("operation: " + operation.getType() + " " + operation.getPosition() + " " + operation.getValue()[0]);
+        System.out.println("operation note after execution: " + operation.getType() + " " + operation.getPosition() + " " + operation.getValue()[0]);
+        return SharedFile.getInstance().getText();
     }
 }
